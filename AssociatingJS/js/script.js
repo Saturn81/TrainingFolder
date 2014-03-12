20f2d0cf752d857001e8da7a4c0dd0c6baa6d77f
Jeremy Andrew Suika <jsuika2@fullsail.edu>
2014-03-11 23:17:15 -0400

    concat. strings

--- AssociatingJS/js/script.js
+++ AssociatingJS/js/script.js
@@ -1,8 +1,9 @@
-//Expressions- Expressions with Arrays
+//Expressions- Expressions with Strings
 
-var orangeBins = [234,567,883];
 
-var total = orangeBins[0]+ orangeBins[1]+orangeBins[2];
+var a ="6";
+var b ="7";
 
-console.log(total);
+var result =a +b;
 
+console.log(result);
