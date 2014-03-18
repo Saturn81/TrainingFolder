1f9aa45a2c4ab34086011eb4df21db59bb611a80
Jeremy Andrew Suika <jsuika2@fullsail.edu>
2014-03-18 12:30:54 -0400

    change

--- AssociatingJS/js/script.js
+++ AssociatingJS/js/script.js
@@ -1,11 +1,13 @@
 //Conditional Logic-Relational Expressions
 
-var kidHeight=47;
+var kidHeight=48;
 var minHeight=48;
-var sneakerLift=2
+
 
 //if the child is over 48 inch in height he can ride the ride
 
-if(kidHeight+sneakerLift>minHeight) {
+if(kidHeight >= minHeight) {
     console.log("You can ride the coaster!");
+}else{
+    console.log("Sorry kid you got some growing to do, boy");
 }
