498301efa82fe9acf977bd53b381f560619a24a7
Jeremy Andrew Suika <jsuika2@fullsail.edu>
2014-03-18 12:54:11 -0400

    ternary after change

--- AssociatingJS/js/script.js
+++ AssociatingJS/js/script.js
@@ -1,11 +1,13 @@
 //Conditioal Logic-Ternary Operators
 
-var gpa =1.8;
+var gpa =5.8;
 
-if(gpa > 2.0){
-    console.log("you are graduating");
+//if(gpa > 2.0){
+   // console.log("you are graduating");
 
-}else{
-    console.log("PGA is low, low low.");
+//}else{
+   // console.log("PGA is low, low low.");
 
-}
+//}
+
+(gpa >2.0) ?  console.log("you are graduating") : console.log("PGA is low, low low.");
