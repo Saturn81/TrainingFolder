2afa0d4942fd98e249be94d06512bb3c33c560d3
Jeremy Andrew Suika <jsuika2@fullsail.edu>
2014-03-18 12:07:23 -0400

    conditional with expressions

--- AssociatingJS/js/script.js
+++ AssociatingJS/js/script.js
@@ -1,10 +1,11 @@
 //Conditional Logic-Relational Expressions
 
-var kidHeight= prompt("Enter in the Kids height");
+var kidHeight=47;
 var minHeight=48;
+var sneakerLift=2
 
 //if the child is over 48 inch in height he can ride the ride
 
-if(kidHeight>minHeight) {
+if(kidHeight+sneakerLift>minHeight) {
     console.log("You can ride the coaster!");
 }
